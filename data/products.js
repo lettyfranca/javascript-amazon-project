165779b67ead1eeb15be139f2789ce2164ecb9d2
Letícia França <leeffrance@gmail.com>
2025-03-19 16:55:42 -0300

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
     products.forEach((product) => {
@@ -9,8 +11,32 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStartUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  
+  getPrice() {
+    return `$${(formatCurrency(this.priceCents))}`;
+  }
+}
+
 export const products = [
-  {
+{
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -695,4 +721,6 @@ export const products = [
       "rain",
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
